3387b9a8175c7fcb5cdb153e99675c38e531e0e0
Usamah <usamahzaheer2@gmail.com>
2023-03-06 23:15:17 +0000

    Update

--- Mask-RCNN/check_gpu.py
+++ Mask-RCNN/check_gpu.py
@@ -10,7 +10,7 @@ from imgaug import augmenters as iaa
 from tqdm import tqdm
 import pandas as pd 
 import glob
-from sklearn.model_selection import KFold
- 
+from sklearn.model_selection import KFold 
 from tensorflow.python.client import device_lib
+
 print(device_lib.list_local_devices())
